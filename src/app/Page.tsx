88fb6ede40370420f729ba4a2c4dc81ab36c0295
Anthony <102490646+Drakeze@users.noreply.github.com>
2025-06-13 16:17:18 -0700

    adding

--- src/app/Page.tsx
+++ src/app/Page.tsx
@@ -1,13 +1,18 @@
-imprort react from react
-import {UseState} from react
-import './layout.jsx'
-import Link from 'next/link'
-import { headers, cookies } from 'next/headers';
-import { GetServerSideProps } from 'next'
+'use client' 
+//imprort react from react
+//import {UseState} from react
+import './layout.tsx'
+import './todoList.tsx'
+import './todoItem.tsx'
+import './todoForm.tsx'
+//import Link from 'next/link'
+//import { headers, cookies } from 'next/headers';
+//import { GetServerSideProps } from 'next'
 
 export interface Todoinerface{
   id: string;
-  name: string:
+  name: string;
+  description: string;
   doneWith: boolean;
 }
  
