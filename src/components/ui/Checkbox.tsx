4ae27911bc05cdf80ee28777bf5be0a91f0db242
Anthony <102490646+Drakeze@users.noreply.github.com>
2025-06-15 22:21:17 -0700

    checkbox
    Worked on the ui (checkbox) did props and setup eeverything

--- src/components/ui/Checkbox.tsx
+++ src/components/ui/Checkbox.tsx
@@ -1,11 +1,30 @@
 'use client'
 impport React from React
 
+// First set of props and using a interface we have a set color amount 
 interface checkBox{
   checked: boolean;
   onChange: (checked: boolean) => void;
   label?: string;
   disabled?: boolean;
+  color?; 'green'| 'red':
 }
 
-const Checkbox: React.FC<checkBox> = ({ checked, onChange, label, disabled = false }
+const Checkbox: React.FC<checkBox> = ({ checked, onChange, label, disabled = false, color = 'green' }) =>{
+  const accentColor = color === 'red' ? 'accent-red-500' : 'accent-green-500';
+}
+// set everything and did colors (green and red and accent color )
+  return(
+    <label className="flex items-center gap-2 cursor-pointer text-white">
+      <input
+      //These are the props and connecting to the interface props above
+        type = "checked"
+        checked = {checked}
+       onChange={(e) => onChange(e.target.checked)}
+       disabled={disabled}
+       // {accentColor} is a JavaScript variable from props that holds the color (green or red) 
+    className={`w-4 h-4 ${accentColor} cursor-pointer`}
+  />
+  {label && <span className="text-blue-900">{label}</span>}
+</label>
+
