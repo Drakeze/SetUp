5b8684082dbec46e408cd725eb0fda9eeab6d04d
Anthony <102490646+Drakeze@users.noreply.github.com>
2025-06-18 15:54:58 -0700

    Started UI for Input

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -4,6 +4,20 @@ import React from React
 //This is the first interface props
 
 interface inputPops{
-  
-
+  value: string;
+  onChange: (value: string) => void;
+  placeholder?: string;
+  disabled?: boolean;
+  type?: 'text' | 'email' | 'password' | 'number';
 }  
+// This where the JS variable comes 
+//adding in the props to  the const 
+
+const Input: React.FC<inputPops> = ({value, onChange, placeholder = '', disabled = false, type = 'text'}) => {
+
+  return(
+    
+  )
+}
+
+
